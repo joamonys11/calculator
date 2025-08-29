d0447c1048b7119cc9c39e836d1cee534d3d14e5
joamonys11 <chormaleesorasith@gmail.com>
2025-08-29 21:30:38 +0200

    Add power-new branch

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # main.py
 from math_utils import add, subtract, multiply, divide
 
-print("Result:", add(2, 3))
-print("Result:", subtract(5, 2))
-print("Result:", multiply(4, 3))
-print("Result:", divide(10, 2))
+if __name__ == "__main__":
+    input_data = input("Enter two numbers separated by space: ")
+    a, b = map(float, input_data.split())
+
+    print(f"Addition: {add(a, b)}")
+    print(f"Subtraction: {subtract(a, b)}")
+    print(f"Multiplication: {multiply(a, b)}")
+    print(f"Division: {divide(a, b)}")
+
--- math_utils.py
+++ math_utils.py
@@ -12,3 +12,6 @@ def divide(a,b):
 
 def multiply(a,b):
     return a * b
+
+def power(a,b):
+    return a ** b
