98be0fbfda7c47e573debc3c748c438b5865486c
joamonys11 <chormaleesorasith@gmail.com>
2025-08-29 21:41:14 +0200

    Add new featur  Modul, Fac , Sqaure

--- math_utils.py
+++ math_utils.py
@@ -15,3 +15,18 @@ def multiply(a,b):
 
 def power(a,b):
     return a ** b
+
+
+def modulus(a,b):
+    return a % b
+
+def floor_divide(a,b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a // b
+
+
+def sqrt(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return a ** 0.5
