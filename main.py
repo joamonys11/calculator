33d4ab21948f9f9af8cefbc5dae5d8768d6dcc06
joamonys11 <chormaleesorasith@gmail.com>
2025-08-29 21:26:46 +0200

    First function

---
+++ main.py
@@ -0,0 +1,7 @@
+# main.py
+from math_utils import add, subtract, multiply, divide
+
+print("Result:", add(2, 3))
+print("Result:", subtract(5, 2))
+print("Result:", multiply(4, 3))
+print("Result:", divide(10, 2))
