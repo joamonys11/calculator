b5e330a49bfc551a22aa4630b3b0ea12230393aa
joamonys11 <chormaleesorasith@gmail.com>
2025-10-27 15:51:40 +0100

    Add Complete State

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ print("Result:", add(2, 3))
 print("Result:", subtract(5, 2))
 print("Result:", multiply(4, 3))
 print("Result:", divide(10, 2))
+print("Program complete")  # This will raise an exception
