590d329af228e9c5a9d550f4e4fb06eb2f3ff27d
joamonys11 <chormaleesorasith@gmail.com>
2025-10-27 16:09:13 +0100

    Feature-message

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ print("Result:", subtract(5, 2))
 print("Result:", multiply(4, 3))
 print("Result:", divide(10, 2))
 print("Program complete")  # This will raise an exception
+print("This line will not be executed")
